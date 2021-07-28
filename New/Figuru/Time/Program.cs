5f772ac86ce0f031da236c1f6cddeb18987eee3c
andrewved92 <87827229+andrewved92@users.noreply.github.com>
2021-07-28 13:57:11 +0300

    Add files via upload

---
+++ New/Figuru/Time/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                int a = 345;
+                
+                object b = a;
+                Console.WriteLine(b);
+                Console.WriteLine(stopwatch.ElapsedTicks);
+
+                stopwatch.Stop();
+               
+                stopwatch.Start();
+                int j = (int)b;
+             
+                stopwatch.Stop();
+
+                Console.WriteLine(b);
+
+                Console.WriteLine(stopwatch.ElapsedTicks);
+            }
+        }
+    }
+}
+
