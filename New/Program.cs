423656f953bf186cef63140598d20b55556596ad
andrewved92 <87827229+andrewved92@users.noreply.github.com>
2021-07-28 12:40:04 +0300

    Add files via upload

---
+++ New/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle test = new Triangle();
+            test.KolGraney = 2;
+            test.DlinaGraney = 1;
+            test = Poschitat(test);
+            Console.WriteLine(test.PloshadFiguru);
+            Console.Read();
+        }
+        public static Triangle Poschitat(Triangle triangle)
+        {
+            triangle.PloshadFiguru = triangle.DlinaGraney * triangle.KolGraney;
+            return triangle;
+        }
+        }
+    }
+
---
+++ New/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+     public struct Triangle
+    {
+       
+        public int KolGraney { get; set; }
+        public int DlinaGraney { get; set; }
+        public int PloshadFiguru { get; set; }
+
+    }
+}
