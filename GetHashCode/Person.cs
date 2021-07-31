a22e3f80a5cefa122b768351c8caba4d9da7375f
andrewved92 <87827229+andrewved92@users.noreply.github.com>
2021-07-31 19:50:37 +0300

    Add files via upload

---
+++ GetHashCode/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public class Person
+    {
+        public int Birth { get; set; }
+        public string Placeofbirth { get; set; }
+        public int PassportId { get; set; }
+        public string FIO { get; set; }
+        public Person(int birth, string placeofbirth, int passportid, string fio)
+        {
+            Birth = birth;
+            Placeofbirth = placeofbirth;
+            PassportId = passportid;
+            FIO = fio;
+        }
+        public override int GetHashCode()
+        {
+            return Birth + PassportId;
+
+        }
+        public override string ToString()
+        {
+            return Placeofbirth + FIO;
+        }
+       
+            }
+           
+        }
+    
+
+
---
+++ GetHashCode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(2000, "Tiraspol", 1, "Vedenchuk Andrew");
+            Person person1 = new Person(2000, "Tiraspol", 2, "Shevshenko Vlad");
+            Person person2 = new Person(2001, "Tiraspol", 3, "Koval Oleg");
+            Person person3 = person;
+            var hash1 = person.GetHashCode();
+            var hash2 = person1.GetHashCode();
+            var hash3 = person2.GetHashCode();
+        
+            
+        
+           
+        
+            
+
+            }
+
+        }
+       
+        }
+    
+
