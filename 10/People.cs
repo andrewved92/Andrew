ce515b89abd0c0e7944bac2fe1fb7336c79a5d8a
andrewved92 <87827229+andrewved92@users.noreply.github.com>
2021-07-31 19:43:59 +0300

    Add files via upload

---
+++ 10/People.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class People
+    {
+        public int Birth { get; set; }
+        public string Placeofbirth { get; set; }
+        public int PassportId { get; set; }
+        public string FIO { get; set; }
+        
+
+        public People(int birth, string placeofbirth, int passportid, string fio)
+        {
+            Birth = birth;
+            Placeofbirth = placeofbirth;
+            PassportId = passportid;
+            FIO = fio;
+            
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is People))
+            {
+                return false;
+            }
+            People result = (People)obj;
+            return result.Birth == Birth && result.PassportId == PassportId;
+        }
+        public static bool operator ==(People one, People two)
+        {
+            return one.Equals(two);
+        }
+        public static bool operator !=(People one, People two)
+        {
+            return !one.Equals(two);
+        }
+    }
+
+}
---
+++ 10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People people = new People(2000, "Tiraspol", 1, "Vedenchuk Andrew");
+            People people1 = new People(2000, "Tiraspol", 2, "Shevchenko Vlad");
+            People people2 = new People(2000, "Tiraspol", 3, "Koval Oleg");
+            people.Equals(people1);
+            people.Equals(people2);
+        }
+    }
+}
