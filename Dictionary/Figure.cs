172a975e82452ffaf2b75daff999d98907af2f6e
andrewved92 <87827229+andrewved92@users.noreply.github.com>
2021-08-03 21:58:31 +0300

    Add files via upload

---
+++ Dictionary/Figure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp14
+{
+  public class Figure
+    {
+        public string Name { get; set; }
+        public char  ID { get; set; }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, Figure> figures = new Dictionary<char, Figure>();
+            figures.Add('t', new Figure() { Name = "Triangle" });
+            figures.Add('s', new Figure() { Name = "Square" });
+            figures.Add('p', new Figure() { Name = "Polygon" });
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            foreach (KeyValuePair<char, Figure> keyValue in figures)
+            {
+                stopwatch.Stop();
+                TimeSpan t = stopwatch.Elapsed;
+                Console.WriteLine($"{figures}. Operation time: {t}");
+                Console.WriteLine( keyValue.Key + " - " + keyValue.Value.Name, $"{figures}");
+               
+
+            }
+
+        }
+    }
+}
+
